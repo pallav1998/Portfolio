954bcb247aa29c28ef47b082d87d4c974797da97
pallav1998 <jain.pallav1998@gmail.com>
2021-09-04 20:50:35 +0530

    adding project page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import Pre from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
+// import Resume from "./components/Resume/Resume";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "./App.css";
@@ -26,14 +26,14 @@ function App() {
 
   return (
     <Router>
-      <Preloader load={load} />
+      <Pre load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
-          {/* <Route path="/project" component={Projects} /> */}
           {/* <Route path="/resume" component={Resume} /> */}
         </Switch>
         <Footer />
